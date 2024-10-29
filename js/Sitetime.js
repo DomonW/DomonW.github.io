a0853d526b5fcc6f49e7219b5028646484341237
Domon <3105417634@qq.com>
2024-10-29 17:07:57 +0800

    1

--- js/Sitetime.js
+++ js/Sitetime.js
@@ -15,7 +15,7 @@ function siteTime() {
   var todaySecond = today.getSeconds();
 
 
-  var t1 = Date.UTC(2021, 10, 24, 00, 00, 00);
+  var t1 = Date.UTC(2021, 10, 24, 0, 0, 0);
   var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
   var diff = t2 - t1;
   var diffYears = Math.floor(diff / years);
@@ -23,6 +23,6 @@ function siteTime() {
   var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
   var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
   var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds);
-  document.getElementById("sitetime").innerHTML = "已运行 " + diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
+  document.getElementById("sitetime").innerHTML = "已运行 " + diffYears + " 年 " + diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 ";
 }
 siteTime();
